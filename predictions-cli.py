5aa6bd9c310541d24f70f43c0bb9c5b955cb170c
Brock Matthews <brockromanmatthews@gmail.com>
2022-10-06 21:06:41 +1300

    get_broadcaster_id

--- predictions-cli.py
+++ predictions-cli.py
@@ -1,6 +1,6 @@
 """
-python predictions-cli.py -c a7f84cb "Does he win?" yes no
-python predictions-cli.py --create 236472364 Win? "he wins!" "No shot." "what?"
+python predictions-cli.py -c a7f84cb "Does he win?" 120 yes no
+python predictions-cli.py --create 236472364 Win? "he wins!" 30 "No shot." "what?"
 python predictions-cli.py -n a7f84cb
 python predictions-cli.py --cancel 236472364
 python predictions-cli.py -r a7f84cb 1
@@ -32,25 +32,47 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='CLI interface for Twitch predictions')
-    parser.add_argument('-c', '--create', type=str, nargs='+', help='Create a prediction: -c <unique_id> <title(:25)> <outcome_1(:25)> <outcome_2(:25)> ... <outcome_10(:25)>')
+    parser.add_argument('-c', '--create', type=str, nargs='+', help='Create a prediction: -c <unique_id> <title(:25)> <prediction window seconds:1-1800> <outcome_1(:25)> <outcome_2(:25)> ... <outcome_10(:25)>')
     parser.add_argument('-l', '--lock', type=str, nargs=1, help='Lock a prediction: -l <id>')
     parser.add_argument('-n', '--cancel', type=str, nargs=1, help='Cancel a prediction: -n <id>')
     parser.add_argument('-r', '--resolve', type=str, nargs=2, help='Resolve a prediction: -r <id> <outcome_index>')
     args = parser.parse_args()
 
     if args.create:
-        if len(args.create) < 4:
-            parser.error('Create requires at least 4 arguments')
-        else:
-            #create_prediction(args.create[0], args.create[1], args.create[2:])
-            print(f'Create prediction: {args.create}')
+        if len(args.create) < 5:
+            parser.error('Create requires at least 5 arguments')
+
+        if len(args.create) > 13:
+            parser.error('Create takes at most 13 arguments')
+
+        try: 
+            args.create[2] = int(args.create[2])
+
+        except ValueError:
+            parser.error('Prediction window must be an integer in the interval [1, 1800]')
+
+        if args.create[2] < 1 or args.create[2] > 1800:
+            parser.error('Prediction window must be an integer in the interval [1, 1800]')
+
+        #create_prediction(args.create)
+        print(f'Create prediction: {args.create}')
+
 
     elif args.cancel:
         #cancel_prediction(args.cancel)
         print(f'Cancel prediction: {args.cancel}')
 
     elif args.resolve:
-        #resolve_prediction(args.resolve[0], args.resolve[1])
+        try:
+            args.resolve[1] = int(args.resolve[1])
+
+        except ValueError:
+            parser.error('Outcome index must be an integer in the interval [0, 9]')
+
+        if args.resolve[1] < 0 or args.resolve[1] > 9:
+            parser.error('Outcome index must be an integer in the interval [0, 9]')
+
+        #resolve_prediction(args.resolve)
         print(f'Resolve prediction: {args.resolve}')
 
     elif args.lock:
